fd00e8358dc6469bf1346a8bc66713ed53cfdcaa
Samael509 <pavel718234@gmail.com>
2026-01-21 14:41:31 +0300

    learn sealed-class

--- src/example/SealedClass.kt
+++ src/example/SealedClass.kt
@@ -20,12 +20,32 @@ fun handleResult(result: NetworkResult){
     }
 }
 
-fun main(){
-    val success = NetworkResult.Success("данные получены")
-    val error = NetworkResult.Error("сервер не отвечает", 500)
-    val loading = NetworkResult.Loading
+sealed class OrderStatus {
+    object Created : OrderStatus()
+    object Paid : OrderStatus()
+    object Shipped : OrderStatus()
+    data class Cancelled(val reason: String) : OrderStatus() // reason
+}
 
-    handleResult(success)
-    handleResult(error)
-    handleResult(loading)
+fun handleOrder(status: OrderStatus){
+    when (status) {
+        OrderStatus.Created -> println("заказ создан")
+        OrderStatus.Paid -> println("заказ оплачен")
+        OrderStatus.Shipped -> println("заказ отправлен")
+        is OrderStatus.Cancelled -> println("заказ отменён: ${status.reason}")
+    }
+}
+
+fun main(){
+//    val success = NetworkResult.Success("данные получены")
+//    val error = NetworkResult.Error("сервер не отвечает", 500)
+//    val loading = NetworkResult.Loading
+//
+//    handleResult(success)
+//    handleResult(error)
+//    handleResult(loading)
+    handleOrder(OrderStatus.Created)
+    handleOrder(OrderStatus.Paid)
+    handleOrder(OrderStatus.Shipped)
+    handleOrder(OrderStatus.Cancelled("нет товара"))
 }
